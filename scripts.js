14be452dfc0d3c5776c6f4b451401737ded77ddd
kimiinglet <56975796+kimiinglet@users.noreply.github.com>
2019-12-20 23:58:33 -0600

    Final

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ var currentDayEl = $("#currentDay");
 var saveBtnEl = $(".saveBtn");
 var textBoxEl = $(".time-block");
 
+var desc1El =$("#desc09");
+
 
 currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
@@ -22,14 +24,41 @@ var update = function () {
 }
 setInterval(update, 1000);
 
+//function to retrieve stored user data
+renderUserInput();
+function renderUserInput() {
+    var userDesc1 = localStorage.getItem("desc09");
+    $("#desc09").val(JSON.parse(userDesc1));
+    console.log(userDesc1);
+    if(userDesc1 === null){
+        return;
+    }
+    desc1El.textContent = userDesc1;
+}
+// event listener to store user input upon click of Save button
+saveBtnEl.on("click", function (event) {
+    event.preventDefault();
+    var input = $("#desc09").val();
+    localStorage.setItem("desc09", JSON.stringify(input));
+    renderUserInput();
+});
 
-// localStorage 
-// $("button").on("click", function (event) {
-//     // alert("Button Clicked!");
-//     event.preventDefault();
-//     var input = $("input").val();
-//     console.log(input);
-//     localStorage.setItem("input", JSON.stringify(input));
-// });
 
-// To clear use localStorage.clear()
+saveBtnEl.on("click", function(event) {
+    event.preventDefault();
+    var input = $()
+})
+
+
+
+localStorage 
+$("button").on("click", function (event) {
+    // alert("Button Clicked!");
+    event.preventDefault();
+    var input = $("input").val();
+    console.log(input);
+    localStorage.setItem("input", JSON.stringify(input));
+    localStorage.clear();
+});
+
+//To clear use localStorage.clear()
